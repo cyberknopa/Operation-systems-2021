5ecdfe1d69cfdb653343a548e80faeb32ac4386a
Veronica Gavrilova <81513344+cyberknopa@users.noreply.github.com>
2021-12-07 15:10:31 +0300

    Create 2-fat.sh

---
+++ Lab5/2-fat.sh
@@ -0,0 +1,20 @@
+dd of=rand.bin if=/dev/urandom bs=1M count=128
+for fs in fat;
+do
+    echo "___Fs is $fs";
+    dd if=/dev/zero of=$fs.bin bs=1M count=512;
+    mkfs -t $fs -F $fs.bin;
+    mkdir -p m/$fs;
+    mount $fs.bin m/$fs;
+    cp rand.bin m/$fs;
+    cd m/$fs;
+    time echo rand.bin > /dev/null;
+    time cp rand.bin testcp.bin;
+    time rm rand.bin;
+    cd ..
+    unmount $fs;
+    cd ..
+    rm -rf $fs;
+    rm -rf $fs.bin;
+done
+rm rand.bin
